72648e50141a67a81b831d72418545c8029b9e6a
AAstrup <alexander.westi.astrup@gmail.com>
2026-06-21 21:35:41 +0200

    Change to validate as I configure cloudflare to cache html based on nextjs header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
+export const revalidate = 60;
+
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
       <body className="min-h-full">
         <div className="flex min-h-full">
           <aside className="w-64 border-r border-black/10 p-6">
